074d3150f1a773129f25080ecea3e0fb2ba22670
Mike Oram <Mikeoram01@gmail.com>
2022-11-15 12:19:41 +0000

    Completed project from Codecademy's IDE

---
+++ console_creatures/Program.cs
@@ -0,0 +1,31 @@
+{
+  class Program
+  {
+    static void Main()
+    {
+      //This program will output a ghost to the console using Console.WriteLine
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("- Casper the Friendly Ghost");
+      Console.WriteLine("c_   ___");
+      Console.WriteLine("  \\\\__|__|__");
+      Console.WriteLine("　　 \\( o_o)");
+      Console.WriteLine("　　　 > ~  >");
+      Console.WriteLine("　　　/ 　 / \\\\");
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/");
+      Console.WriteLine("　　/　 /");
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\ ");
+      Console.WriteLine("　|　|  \\ ");
+      Console.WriteLine("　| / \\ )");
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+      Console.WriteLine("- Dancing Man");
+     }
+  }
+}
